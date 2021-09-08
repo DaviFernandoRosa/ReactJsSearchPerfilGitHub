77ab5930acc4e5851bfb6d929dcb2a63f77a7f7d
Davi Fernando Rosa <davir0514@gmail.com>
2021-09-07 22:18:11 -0300

    Arquivo

---
+++ src/components/container/styles.js
@@ -0,0 +1,13 @@
+
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    width: 100%;
+    height: 100vh;
+    background: #121212;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    
+`
---
+++ src/components/userContainer/styles.js
@@ -0,0 +1,11 @@
+
+import styled from 'styled-components';
+
+export const Section = styled.section`
+    width: 400px;
+    height: 300px;
+    background: rgba(255,255,255,.95);
+    border-radius: 20px;
+    border-bottom: 15px solid #8564c1;
+    margin-top: 20px;
+`
